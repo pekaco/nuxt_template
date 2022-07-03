6c30c4728d35231f6d47a77245f9f3914aa7ae27
pekaco <mari.ktmr.pekaco@gmail.com>
2022-07-04 04:06:21 +0900

    chore: eslint周りのセットアップ

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        htmlWhitespaceSensitivity: "strict",
+      },
+    ],
+    "vue/multi-word-component-names": "off",
+  },
+};
